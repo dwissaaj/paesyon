85e997d33657c9c46f73b1a69a91b1dc254e5b7e
dwisaji <dwis.buntelman123@gmail.com>
2022-06-21 19:00:17 +0700

    deployment branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
+import Article from './Article/Article';
+import Recomendation from './Recomendation/Recomendation';
+
 
 function App() {
+  
   return (
     <div className="App">
-      <p>REACT APP FOR PAESYON</p>
+      <Recomendation/>
+      <Article/>
     </div>
   );
 }
