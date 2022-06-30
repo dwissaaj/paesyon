f198274a73a309ce465de0ee45340de484170f91
kevinusmayadhyw <57991938+kevinusmayadhyw@users.noreply.github.com>
2022-06-30 13:32:58 +0700

    install icon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-
-import './App.css';
-import Article from './Article/Article';
-import Recomendation from './Recomendation/Recomendation';
-import BreakSection from './BreakSection/BreakSection';
-import NavigationBar from './Navbar/NavigationBar';
-import Contact from './Contact us/Contact';
-import Footer from './footer/Footer';
-import AboutWeb from './About Us/AboutWeb';
-import AboutTeam from './About Us/AboutTeam';
+import "./App.css";
+import Article from "./Article/Article";
+import Recomendation from "./Recomendation/Recomendation";
+import BreakSection from "./BreakSection/BreakSection";
+import NavigationBar from "./Navbar/NavigationBar";
+import Contact from "./Contact us/Contact";
+import Footer from "./footer/Footer";
+import AboutWeb from "./About Us/AboutWeb";
+import AboutTeam from "./About Us/AboutTeam";
 function App() {
   return (
     <div className="App">
-        <NavigationBar />
-            <AboutWeb/>
-            <BreakSection/>
-            <AboutTeam/>
-            <Recomendation/>
-            <Article/>
-            <Contact/>
-            <Footer/>
+      <NavigationBar />
+      <AboutWeb />
+      <BreakSection />
+      <AboutTeam />
+      <Recomendation />
+      <Article />
+      <Contact />
+      <Footer />
     </div>
   );
 }
