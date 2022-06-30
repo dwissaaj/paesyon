a9ea46a42e2f1cd29a164de718b5451daa0dacb1
dwisaji <dwisetyoaji@jrdataanlyst.com>
2022-06-30 12:49:38 +0700

    fix bug single article

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 
 import './App.css';
-import LandingPage from './Pages/LandingPage';
-import { Routes, Route} from 'react-router-dom'
-
+import Article from './Article/Article';
+import Recomendation from './Recomendation/Recomendation';
+import BreakSection from './BreakSection/BreakSection';
+import NavigationBar from './Navbar/NavigationBar';
+import Contact from './Contact us/Contact';
+import Footer from './footer/Footer';
+import AboutWeb from './About Us/AboutWeb';
+import AboutTeam from './About Us/AboutTeam';
 function App() {
   return (
     <div className="App">
-        <Routes>
-          <Route path='/home' element={<LandingPage/>} />
-        </Routes>
+        <NavigationBar />
+            <AboutWeb/>
+            <BreakSection/>
+            <AboutTeam/>
+            <Recomendation/>
+            <Article/>
+            <Contact/>
+            <Footer/>
     </div>
   );
 }
